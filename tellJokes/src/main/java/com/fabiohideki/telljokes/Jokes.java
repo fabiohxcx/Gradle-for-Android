1cc96d4ff05dd2f32cd9283d7c9ba244286729c4
fabiohxcx <hideki.fabio@gmail.com>
2018-01-12 14:26:04 -0200

    Step 1: lib Java

---
+++ tellJokes/src/main/java/com/fabiohideki/telljokes/Jokes.java
@@ -0,0 +1,12 @@
+package com.fabiohideki.telljokes;
+
+public class Jokes {
+
+    public static String getJoke() {
+        return "I can't believe I made it anywhere creatively, though, because I was raised by two loving and supportive parents. " +
+                "Nothing squashes creativity more than unconditional love and support from a functional household. If you have kids, sh*t " +
+                "on their dreams a little bit.";
+    }
+
+
+}
