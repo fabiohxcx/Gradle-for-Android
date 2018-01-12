cc998954c597f3574d55f1edd076ce3854cc89ad
fabiohxcx <hideki.fabio@gmail.com>
2018-01-12 16:47:22 -0200

    Step 2: Creating an Android Library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,12 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
+import com.fabiohideki.androidlibrary.JokeActivity;
 import com.fabiohideki.telljokes.Jokes;
 
 
@@ -42,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, Jokes.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(this, JokeActivity.class);
+        myIntent.putExtra(JokeActivity.EXTRA_JOKE, Jokes.getJoke());
+        startActivity(myIntent);
     }
 
 
--- tellJokes/src/main/java/com/fabiohideki/telljokes/Jokes.java
+++ tellJokes/src/main/java/com/fabiohideki/telljokes/Jokes.java
@@ -3,9 +3,9 @@ package com.fabiohideki.telljokes;
 public class Jokes {
 
     public static String getJoke() {
-        return "I can't believe I made it anywhere creatively, though, because I was raised by two loving and supportive parents. " +
-                "Nothing squashes creativity more than unconditional love and support from a functional household. If you have kids, sh*t " +
-                "on their dreams a little bit.";
+        return "A man asks a farmer near a field, “Sorry sir, would you mind if I crossed your field instead of going around it? You see, I have to catch the 4:23 train.”\n" +
+                "\n" +
+                "The farmer says, “Sure, go right ahead. And if my bull sees you, you’ll even catch the 4:11 one.”\n";
     }
 
 
