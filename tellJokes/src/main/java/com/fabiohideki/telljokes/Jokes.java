460c5ec105275285f1faca2cfb2e204a9b6dc99b
fabiohxcx <hideki.fabio@gmail.com>
2018-01-15 16:37:08 -0200

    GCE changes

--- tellJokes/src/main/java/com/fabiohideki/telljokes/Jokes.java
+++ tellJokes/src/main/java/com/fabiohideki/telljokes/Jokes.java
@@ -3,9 +3,9 @@ package com.fabiohideki.telljokes;
 public class Jokes {
 
     public static String getJoke() {
-        return "A man asks a farmer near a field, “Sorry sir, would you mind if I crossed your field instead of going around it? You see, I have to catch the 4:23 train.”\n" +
+        return "A man asks a farmer near a field, Sorry sir, would you mind if I crossed your field instead of going around it? You see, I have to catch the 4:23 train.\n" +
                 "\n" +
-                "The farmer says, “Sure, go right ahead. And if my bull sees you, you’ll even catch the 4:11 one.”\n";
+                "The farmer says, Sure, go right ahead. And if my bull sees you, you’ll even catch the 4:11 one.";
     }
 
 
